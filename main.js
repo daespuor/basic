8a1cf2ce042eab814c03f028898a4189290f712f
Daniel Esteban <daespuor1991@gmail.com>
2020-03-09 20:58:34 -0500

    My initial JAMStack App

---
+++ main.js
@@ -0,0 +1,16 @@
+const fistRepos = async username =>{
+    const repos = await fetch(`https://api.github.com/users/${username}/repos?type=owner&sort=updated`)
+                        .then(repos => repos.json())
+                        .catch(error=>console.error(error));
+    const markup = repos.map(repo => `
+        <li>
+            <a href="${repo.html_url}">${repo.name}</a>
+            ( 🌟 ${repo.stargazers_count})
+        </li>
+    `).join('');
+    const content= document.getElementById('content');
+    content.innerHTML=`<ul>${markup}</ul>`
+
+}
+
+fistRepos('daespuor')
